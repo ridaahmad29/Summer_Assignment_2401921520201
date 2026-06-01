5a5c9f7ed3a903a1d091ae9cc3db5d272bf72700
RIda Ahmad <ridaahmad2206@gmail.com>
2026-06-01 20:59:01 +0530

    WEEK 1 - DAY 2 Solutions

---
+++ Week_1/Day_2/MaxSubarray.java
@@ -0,0 +1,14 @@
+public int maxSubarray(int[] nums) {
+    int sum =0 ;
+    int max = Integer.MIN_VALUE;
+    for(int i = 0 ; i < nums.length ; i ++){
+        sum+= nums[i];
+        if(sum> max){
+            max = sum;
+        }
+        if(sum<0){
+            sum = 0;
+        }
+    }
+    return max;
+}
