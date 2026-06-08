590ebea0984be9ce3d31e07bf1ba5eb1825ceae1
RIda Ahmad <ridaahmad2206@gmail.com>
2026-06-08 16:39:30 +0530

    WEEK 2 - DAY 1 Solutions

---
+++ Week_2/Day_1/Anagram.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+}
