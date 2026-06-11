adafe199522c47e4a32a2dd4adf8ee309294bbd7
RIda Ahmad <ridaahmad2206@gmail.com>
2026-06-11 20:35:49 +0530

    Week 2 - Day 5 Solutions

---
+++ Week_2/Day_5/StringCompression.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int compress(char[] chars) {
+        int i =0;
+        int index = 0;
+        while(i<chars.length){
+            char current = chars[i];
+            int count = 0;
+
+            while(i <chars.length && chars[i] ==current){
+                count++;
+                i++;
+            }
+            chars[index++] =current;
+            if(count>1){
+                String cnt = String.valueOf(count);
+
+                for (char c :cnt.toCharArray()) {
+                    chars[index++]=c;
+                }
+            }
+        }
+
+        return index;
+    }
+}
